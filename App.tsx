2d8c75601d8f1ff6d413fd52baaf0f1641d0c766
Jus <justerureta07@gmail.com>
2026-05-29 17:03:55 +0800

    try 2 hehe

--- App.tsx
+++ App.tsx
@@ -1,5 +1,36 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MarketplaceHomeScreen from './MarketplaceHomeScreen';
+import PoultryMart from './poultrymart';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <MarketplaceHomeScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: true,
+          tabBarActiveTintColor: '#111111',
+          tabBarInactiveTintColor: '#888888',
+        }}
+      >
+        <Tab.Screen
+          name="Marketplace"
+          component={MarketplaceHomeScreen}
+          options={{
+            title: 'Marketplace',
+          }}
+        />
+        <Tab.Screen
+          name="PoultryMart"
+          component={PoultryMart}
+          options={{
+            title: 'PoultryMart',
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
